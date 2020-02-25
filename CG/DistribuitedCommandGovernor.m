063dec434c45ae03d4a5caf33bf67202dcf50ca1
Vincenzo D'Angelo <vinz-uts@hotmail.it>
2020-02-25 19:56:44 +0100

    :bug: :construction:
    Verify and fixing bug

--- CG/DistribuitedCommandGovernor.m
+++ CG/DistribuitedCommandGovernor.m
@@ -17,13 +17,15 @@ classdef DistribuitedCommandGovernor < CommandGovernor
             obj = obj@CommandGovernor(Phi,G,Hc,L,T,gi,Psi,k0,delta,dist_flag);
             if nargin > 10
                 % Introducing binary slack variables for OR-ed constraints
-                b = binvar(size(U,1)/2,1);
-                B_diag = [];
-                for i=1:(size(U,1)/2)
-                    B_diag = blkdiag(B_diag,b(i),~b(i));
-                end
-                obj.U = B_diag*U; % proximity constraints matrix
-                obj.hi = B_diag*hi; % proximity constraints vector
+                %b = binvar(size(U,1)/2,1);
+                %B_diag = [];
+                %for i=1:(size(U,1)/2)
+                %    B_diag = blkdiag(B_diag,b(i),~b(i));
+                %end
+                %obj.U = B_diag*U; % proximity constraints matrix
+                %obj.hi = B_diag*hi; % proximity constraints vector
+                obj.U = U;
+                obj.hi = hi;
             end
         end
         
@@ -34,27 +36,38 @@ classdef DistribuitedCommandGovernor < CommandGovernor
             g = sdpvar(length(r),1);
             w = [g;g_n];
             cnstr = ismember((obj.Hc/(eye(size(obj.Phi,1))-obj.Phi)*obj.G+obj.L)*w,obj.C_k);
-            cnstr = [cnstr obj.U*((obj.Hc/(eye(size(obj.Phi,1))-obj.Phi)*obj.G+obj.L)*w)<=obj.hi];
+            for i=1:(size(obj.U,1)/2)
+                cnstr = [cnstr obj.U((i-1)*2+1,:)*((obj.Hc/(eye(size(obj.Phi,1))-obj.Phi)*obj.G+obj.L)*w)<=obj.hi((i-1)*2+1) | ...
+                    obj.U((i-1)*2+2,:)*((obj.Hc/(eye(size(obj.Phi,1))-obj.Phi)*obj.G+obj.L)*w)<=obj.hi((i-1)*2+2)];
+            end
             xk = x;
             if obj.dist_flag == 0
                 % Case: without disturbance
                 for k = 1:obj.k0
                     xk = obj.Phi*xk+obj.G*w;
                     cnstr = [cnstr ismember(obj.Hc*xk+obj.L*w,obj.C_k)];
-                    cnstr = [cnstr obj.U*(obj.Hc*xk+obj.L*w)<=obj.hi];
+                    %cnstr = [cnstr obj.U*(obj.Hc*xk+obj.L*w)<=obj.hi];
+                    for i=1:(size(obj.U,1)/2)
+                         cnstr = [cnstr obj.U((i-1)*2+1,:)*(obj.Hc*xk+obj.L*w)<=obj.hi((i-1)*2+1) | ...
+                         obj.U((i-1)*2+2,:)*(obj.Hc*xk+obj.L*w)<=obj.hi((i-1)*2+2)];
+                    end
                 end
             else
                 % Case: with disturbance
                 for k = 1:obj.k0
                     xk = obj.Phi*xk+obj.G*w;
                     cnstr = [cnstr ismember(obj.Hc*xk+obj.L*w,obj.C_k{k})];
-                    cnstr = [cnstr obj.U*(obj.Hc*xk+obj.L*w)<=obj.hi]; % verify
+                    %cnstr = [cnstr obj.U*(obj.Hc*xk+obj.L*w)<=obj.hi]; % verify
+                    for i=1:(size(obj.U,1)/2)
+                         cnstr = [cnstr obj.U((i-1)*2+1,:)*(obj.Hc*xk+obj.L*w)<=obj.hi((i-1)*2+1) | ...
+                         obj.U((i-1)*2+2,:)*(obj.Hc*xk+obj.L*w)<=obj.hi((i-1)*2+2)];
+                    end
                 end
             end
             % Objective function
             obj_fun = (r-g)'*obj.Psi*(r-g);
             % Solver options
-            options = sdpsettings('verbose',0,'solver','bnb');
+            options = sdpsettings('verbose',0);
             
             solvesdp(cnstr,obj_fun,options);
             g = double(g);
