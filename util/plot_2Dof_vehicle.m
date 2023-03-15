f613760990b3f8f73c979aa89b8750969153ac2a
vinz-uts <vinz-uts@hotmail.it>
2023-03-15 22:14:41 +0100

    🔖 CoGoV
    CoGoV Public Release v.01

--- util/plot_2Dof_vehicle.m
+++ util/plot_2Dof_vehicle.m
@@ -1,3 +1,18 @@
+% Copyright 2021 - CoGoV.
+% Licensed under the Academic Free License, Version 3.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+% https://opensource.org/license/afl-3-0-php/
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+% 
+% Authors: Vincenzo D'Angelo, Ayman El Qemmah, Franco Angelo Torchiaro
+% Credits: Alessandro Casavola, Francesco Tedesco
+
+
 function plot_struct = plot_2Dof_vehicle(vehicle, r, d_min, varargin)
     
     hold_state = ishold;
